aedc2553ca63762c10557980087f260ab4473813
Maimoona M.Irfan <maimonairfan@gmail.com>
2023-03-08 00:40:46 +0500

    SplashScreen

---
+++ lib/views/screen.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:splashscreen/views/login.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: ((context) => Login())));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+        child: Center(
+            child: Text(
+          "Classico",
+          style: TextStyle(
+              fontSize: 34, fontWeight: FontWeight.bold, color: Colors.white),
+        )),
+      ),
+    );
+  }
+}
